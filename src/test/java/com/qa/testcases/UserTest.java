67f58e745410e537bd3e86796c0037da1d183c8a
cyrilgeorge153 <cyrilgeorge153@gmail.com>
2019-12-21 19:57:45 +0530

    testng attributes added

--- src/test/java/com/qa/testcases/UserTest.java
+++ src/test/java/com/qa/testcases/UserTest.java
@@ -2,6 +2,7 @@ package com.qa.testcases;
 
 import java.io.IOException;
 
+import org.apache.log4j.Logger;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
@@ -19,9 +20,11 @@ import com.qa.pages.UserDashboardPage;
  */
 public class UserTest extends BaseClass {
 	public static UserDashboardPage db;
+	Logger logger=Logger.getLogger(BaseClass.class);
 
 	@BeforeMethod()
 	public void setUp() throws IOException {
+		logger.info("*******************starting before method**************************");
 		startBrowser();
 		browserConfig();
 		readUrl();
@@ -30,6 +33,7 @@ public class UserTest extends BaseClass {
 		db.clickTours();
 		db.clickSearch();
 		db.clickDetails();
+		logger.info("*******************ending before method**************************");
 	}
 	
 	@AfterMethod()
@@ -37,9 +41,10 @@ public class UserTest extends BaseClass {
 		closeBrowser();
 	}
 	
-	@Test(priority = 0)
+	@Test(priority = 0,groups = "smoke",enabled = true,description = " To test the full flow of demo user")
 	public void verifyDemoUserFullFlowTest() throws IOException 
 	{
+		logger.info("*******************starting verifyDemoUserFullFlowTest**************************");
 		db.verifyImage();
 		db.verifyFbShare();
 		db.verifyTwitterShare();
@@ -57,109 +62,142 @@ public class UserTest extends BaseClass {
 		db.clickConfirmBooking();
 		db.verifyBookingDetails();
 		db.verifyTotalAmount();
+		logger.info("*******************ending verifyDemoUserFullFlowTest**************************");
 	}
 
-	@Test(priority = 1)
+	@Test(priority = 1,groups = "regression",enabled = true,description = "To verifyImageTest")
 	public void verifyImageTest() throws IOException 
 	{
+		logger.info("*******************starting verifyImageTest**************************");
 		db.verifyImage();
+		logger.info("*******************ending verifyImageTest**************************");
 	}
 
-	@Test(priority = 2)
+	@Test(priority = 2,groups = "regression",enabled = true,description = "To verifyFbShareTest")
 	public void verifyFbShareTest() throws IOException 
 	{
+		logger.info("*******************starting verifyFbShareTest**************************");
 		db.verifyFbShare();
+		logger.info("*******************ending verifyFbShareTest**************************");
 	}
 
-	@Test(priority = 3)
+	@Test(priority = 3,groups = "regression",enabled = true,description = "To verifyTwitterShareTest")
 	public void verifyTwitterShareTest() throws IOException
 	{
+		logger.info("*******************starting verifyTwitterShareTest**************************");
 		db.verifyTwitterShare();
+		logger.info("*******************ending verifyTwitterShareTest**************************");
 	}
 
-	@Test(priority = 4)
+	@Test(priority = 4,groups = "regression",enabled = true,description = "To verifyPininterestShareTest")
 	public void verifyPininterestShareTest() throws IOException 
 	{
+		logger.info("*******************starting verifyPininterestShareTest**************************");
 		db.verifyPininterestShare();
+		logger.info("*******************ending verifyPininterestShareTest**************************");
 	}
 
-	@Test(priority = 5)
+	@Test(priority = 5,groups = "regression",enabled = true,description = "To verifyNormalShareTest")
 	public void verifyNormalShareTest() throws IOException
 	{
+		logger.info("*******************starting verifyNormalShareTest**************************");
 		db.verifyNormalShare();
+		logger.info("*******************ending verifyNormalShareTest**************************");
 	}
 
-	@Test(priority = 6)
+	@Test(priority = 6,groups = "regression",enabled = true,description = "To verifyShowMapTest")
 	public void verifyShowMapTest() throws IOException 
 	{
+		logger.info("*******************starting verifyShowMapTest**************************");
 		db.verifyShowMap();
+		logger.info("*******************ending verifyShowMapTest**************************");
 	}
 
-	@Test(priority = 7)
+	@Test(priority = 7,groups = "regression",enabled = true,description = "To verifyInclusionsTest")
 	public void verifyInclusionsTest() throws IOException 
 	{
+		logger.info("*******************starting verifyInclusionsTest**************************");
 		db.verifyInclusions();
+		logger.info("*******************ending verifyInclusionsTest**************************");
 	}
 
-	@Test(priority = 8)
+	@Test(priority = 8,groups = "regression",enabled = true,description = "To verifyExclusionsTest")
 	public void verifyExclusionsTest() throws IOException 
 	{
+		logger.info("*******************starting verifyExclusionsTest**************************");
 		db.verifyExclusions();
+		logger.info("*******************ending verifyExclusionsTest**************************");
 	}
 
-	@Test(priority = 9)
+	@Test(priority = 9,groups = "regression",enabled = true,description = "To verifyWriteReviewTest")
 	public void verifyWriteReviewTest() throws IOException 
 	{
+		logger.info("*******************starting verifyWriteReviewTest**************************");
 		db.verifyWriteReview();
+		logger.info("*******************ending verifyWriteReviewTest**************************");
 	}
 
-	@Test(priority = 10)
+	@Test(priority = 10,groups = "regression",enabled = true,description = "To verifyChangeDateTest")
 	public void verifyChangeDateTest() throws IOException 
 	{
+		logger.info("*******************starting verifyChangeDateTest**************************");
 		db.verifyChangeDate();
+		logger.info("*******************ending verifyChangeDateTest**************************");
 	}
 
-	@Test(priority = 11)
+	@Test(priority = 11,groups = "smoke",enabled = true,description = "To clickBookNowTest")
 	public void clickBookNowTest() throws IOException 
 	{
+		logger.info("*******************starting clickBookNowTest**************************");
 		db.clickBookNow();
+		logger.info("*******************ending clickBookNowTest**************************");
 	}
 
-	@Test(priority = 12)
+	@Test(priority = 12,groups = "regression",enabled = true,description = "To verifyPersonalDetailsTest")
 	public void verifyPersonalDetailsTest() throws IOException 
 	{
+		logger.info("*******************starting verifyPersonalDetailsTest**************************");
 		db.clickBookNow();
 		db.verifyPersonalDetails();
+		logger.info("*******************ending verifyPersonalDetailsTest**************************");
 	}
 
-	@Test(priority = 13)
+	@Test(priority = 13,groups = "regression",enabled = true,description = "To verifyBookingDateTest")
 	public void verifyBookingDateTest() throws IOException 
 	{
+		logger.info("*******************starting verifyBookingDateTest**************************");
 		db.clickBookNow();
 		db.verifyBookingDate();
+		logger.info("*******************ending verifyBookingDateTest**************************");
 	}
 
-	@Test(priority = 14)
+	@Test(priority = 14,groups = "regression",enabled = true,description = "To verifyAmountTest")
 	public void verifyAmountTest() throws IOException 
 	{
+		logger.info("*******************starting verifyAmountTest**************************");
 		db.clickBookNow();
 		db.verifyAmount();
+		logger.info("*******************ending verifyAmountTest**************************");
 	}
 	
-	@Test(priority = 15)
+	@Test(priority = 15,groups = "regression",enabled = true,description = "To verifyBookingDetailsTest")
 	public void verifyBookingDetailsTest() throws IOException 
 	{
+		logger.info("*******************starting verifyBookingDetailsTest**************************");
 		db.clickBookNow();
 		db.clickConfirmBooking();
 		db.verifyBookingDetails();
+		logger.info("*******************ending verifyBookingDetailsTest**************************");
 	}
 	
-	@Test(priority = 16)
+	@Test(priority = 16,groups = "regression",enabled = true,description = "To verifyTotalAmountTest")
 	public void verifyTotalAmountTest() throws IOException 
 	{
+		logger.info("*******************starting verifyTotalAmountTest**************************");
 		db.clickBookNow();
 		db.clickConfirmBooking();
 		db.verifyTotalAmount();
+		logger.info("*******************ending verifyTotalAmountTest**************************");
 	}
 	
 	
