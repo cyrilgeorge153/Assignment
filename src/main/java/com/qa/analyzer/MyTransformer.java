a4accca2c07dad95ff886a223db4128396c4d7db
Cyril George <cyrilgeorge153@gmail.com>
2019-12-31 11:43:45 +0530

    retry analyzer logic added

---
+++ src/main/java/com/qa/analyzer/MyTransformer.java
@@ -0,0 +1,27 @@
+/**
+ * 
+ */
+package com.qa.analyzer;
+
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Method;
+
+import org.testng.annotations.ITestAnnotation;
+import org.testng.IAnnotationTransformer;
+
+/**
+ * @author CYRIL
+ *
+ */
+public class MyTransformer implements IAnnotationTransformer {
+	
+public void transform(ITestAnnotation annotation,  Class testClass,  Constructor testConstructor, Method testMethod)
+	
+	{
+		
+		annotation.setRetryAnalyzer(RetryAnalyzer.class);
+		
+		
+	}
+
+}
